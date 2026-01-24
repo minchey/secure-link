7330428274fd2e7c85220decc34ad3052b832b38
minchey <nolam@naver.com>
2026-01-25 00:28:08 +0900

    create 메서드 완료

--- src/main/java/com/example/securelink/link/Link.java
+++ src/main/java/com/example/securelink/link/Link.java
@@ -2,6 +2,7 @@ package com.example.securelink.link;
 
 import java.time.LocalDateTime;
 
+
 public class Link {
     private String token = "";
     private LinkStatus status;
--- src/main/java/com/example/securelink/link/LinkService.java
+++ src/main/java/com/example/securelink/link/LinkService.java
@@ -1,12 +1,16 @@
 package com.example.securelink.link;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 public class LinkService {
 
     public Link createLink(int expireTime){
-        LocalDateTime createTime = LocalDateTime.now();
-        LocalDateTime expiresTime = createTime.plusHours(expireTime);
+        String token = UUID.randomUUID().toString();
         LinkStatus status = LinkStatus.CREATED;
+        LocalDateTime createAt = LocalDateTime.now();
+        LocalDateTime expiresAt = createAt.plusHours(expireTime);
+
+        return new Link(token, status, expiresAt);
     }
 }
