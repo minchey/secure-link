7e873f9e331ad04174f5742b95e83f9d3d12231a
minchey <nolam@naver.com>
2026-01-26 22:49:47 +0900

    컨트롤러 post메서드 완성

--- src/main/java/com/example/securelink/link/LinkController.java
+++ src/main/java/com/example/securelink/link/LinkController.java
@@ -1,4 +1,14 @@
 package com.example.securelink.link;
 
 public class LinkController {
+
+    private final LinkService linkService;
+
+    public LinkController(LinkService linkService){
+        this.linkService = linkService;
+    }
+
+    public Link postLink(int expireTime){
+        return linkService.createLink(expireTime);
+    }
 }
